206ec2ce387188f0ac8f8d93c93ea0f599103b37
Mouwafic Badarou <badaroumouwafic@gmail.com>
2024-12-26 17:38:36 +0100

    Add Jumbotron component: implement main structure with Container, Title, SubTitle, and styling

---
+++ src/components/jumbotron/index.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-no-undef */
+import {Inner, Item, Container, Title, SubTitle} from "./styles/jumbotron"
+
+export default function Jumbotron({children, direction = 'row', ...restProps}) {
+  return (
+    <Item {...restProps}>
+      <Inner direction={direction}>{children}</Inner>
+    </Item>
+  )
+}
+
+Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
+  return <Container {...restProps}>{children}</Container>
+}
+Jumbotron.Title = function JumbotronTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>
+}
+Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...restProps }) {
+  return <SubTitle {...restProps}>{children}</SubTitle>
+}
