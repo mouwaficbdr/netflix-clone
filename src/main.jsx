3e306f1d17ea9634b7d2a1bc673abcd64435d724
Mouwafic Badarou <badaroumouwafic@gmail.com>
2024-12-27 05:16:09 +0100

    Integrate GlobalStyles component into main application: add GlobalStyles to main.jsx for consistent styling across the app.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { GlobalStyles } from './global-styles.jsx'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <GlobalStyles/>
     <App />
   </StrictMode>,
 )
