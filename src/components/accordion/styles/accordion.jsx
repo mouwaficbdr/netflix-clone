57d3ea566d4344c8f4d0b17903fa5d131502c4b9
Mouwafic Badarou <badaroumouwafic@gmail.com>
2024-12-27 21:37:25 +0100

    Add styled-components for Accordion: create styled elements (Container, Inner, Title, Header, Body, Item) for improved layout and modularity in accordion.jsx

---
+++ src/components/accordion/styles/accordion.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+
+export const Container = styled.section``
+
+export const Inner = styled.div``
+
+export const Title = styled.h1``
+
+export const Header = styled.div``
+
+export const Body = styled.body``
+
+export const Item = styled.div``
+
