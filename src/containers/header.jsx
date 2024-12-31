f5f44143ea92afc54717121ffef13db425a17aae
Mouwafic Badarou <badaroumouwafic@gmail.com>
2024-12-31 19:53:07 +0100

    Add HeaderContainer component: Introduce a new HeaderContainer in header.jsx that integrates the Header component with routing capabilities. This addition includes a logo and a sign-in button, enhancing navigation and user interaction within the application.

---
+++ src/containers/header.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+import { Header } from "../components"
+import * as ROUTES from "../constants/routes"
+
+export function HeaderContainer({ children }) {
+  return (
+    <Header>
+      <Header.Frame>
+        <Header.Logo to={ROUTES.HOME} src="/images/misc/logo.svg" alt="Netflix"/>
+        <Header.ButtonLink to={ROUTES.SIGN_IN}>Sign  In</Header.ButtonLink>
+      </Header.Frame>
+      {children}
+    </Header>
+  )
+}
