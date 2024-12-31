91d26d8ac6db7b891f248cfdc986d314c30d7e1c
Mouwafic Badarou <badaroumouwafic@gmail.com>
2024-12-31 14:49:11 +0100

    Refactor routing in App component: Replace hardcoded paths with constants from ROUTES for better maintainability and readability. This change enhances the structure of the routing logic in the application.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import * as ROUTES from './constants/routes'
 import Home from "./pages/home"
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/signin" element={<p>I will be the sign in page</p>} />
-        <Route path="/signup" element={<p>I will be the sign up page</p>} />
-        <Route path="/browse" element={<p>I will be the browse page</p>} />
-        <Route path="/" element={<Home />}
+        <Route
+          path={ROUTES.SIGN_IN}
+          element={<p>I will be the sign in page</p>}
         />
+        <Route
+          path={ROUTES.SIGN_UP}
+          element={<p>I will be the sign up page</p>}
+        />
+        <Route
+          path={ROUTES.BROWSE}
+          element={<p>I will be the browse page</p>}
+        />
+        <Route path={ROUTES.HOME} element={<Home />} />
       </Routes>
     </Router>
   );
