ef5e5d86730ed9613449ae47bcc89f686260b7f0
Mouwafic Badarou <badaroumouwafic@gmail.com>
2024-12-31 14:15:24 +0100

    Add Break subcomponent to OptForm: Enhance layout flexibility by introducing a new Break component for better spacing in the OptForm structure.

--- src/components/opt-form/index.jsx
+++ src/components/opt-form/index.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import {Container, Input, Button, Text} from "./styles/opt-form"
+import {Container, Input, Button, Text, Break} from "./styles/opt-form"
 
 export default function OptForm({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>
@@ -14,6 +14,11 @@ OptForm.Button = function OptFormButton({ children, ...restProps }) {
     {children}
     <img src="/images/icons/chevron-right.png" alt="Try Now"/>
   </Button>
+}
+
+
+OptForm.Break = function OptFormBreak({...restProps }) {
+  return <Break {...restProps} />
 }
 OptForm.Text = function OptFormText({ children, ...restProps }) {
   return <Text {...restProps}>{children}</Text>
