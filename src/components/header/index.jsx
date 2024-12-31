ee2a42fce7e052bf461999c70aeb291985d40058
Mouwafic Badarou <badaroumouwafic@gmail.com>
2024-12-31 19:52:39 +0100

    Add Header component: Introduce a new Header component with subcomponents for improved layout and navigation. This update includes a customizable background, a container for children, a logo with routing capabilities, and a button link for enhanced user interaction.

---
+++ src/components/header/index.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/prop-types */
+import { Link as ReachRouterLink } from 'react-router-dom'
+import { Background, Container, Logo, ButtonLink } from './styles/header'
+
+export default function Header({ bg = true, children, ...restProps }) {
+  return bg ? <Background {...restProps}>{children}</Background> : children;
+}
+
+Header.Frame = function HeaderFrame({ children, ...restProps }) {
+  return <Container {...restProps}>{children}</Container>
+}
+
+Header.Logo = function HeaderLogo({ to, ...restProps }) {
+  return (
+    <ReachRouterLink to={to}>
+      <Logo {...restProps} />
+    </ReachRouterLink>
+  )
+}
+
+Header.ButtonLink = function HeaderLogo({ children, ...restProps }) {
+  return <ButtonLink {...restProps}>{children}</ButtonLink>
+}
