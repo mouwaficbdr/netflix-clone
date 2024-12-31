544ad6df9b6886b76534a4bcb88e7e136b08ce87
Mouwafic Badarou <badaroumouwafic@gmail.com>
2024-12-31 14:16:03 +0100

    Enhance FaqsContainer: Add new OptForm button and text for improved user engagement. This update includes a "Try it Now" button and a prompt for users to enter their email to create or restart their membership, enhancing the overall functionality of the form.

--- src/containers/faqs.jsx
+++ src/containers/faqs.jsx
@@ -16,6 +16,9 @@ export function FaqsContainer() {
 
       <OptForm>
         <OptForm.Input placeholder="Email Address" />
+        <OptForm.Button>Try it Now</OptForm.Button>
+        <OptForm.Break />
+        <OptForm.Text>Ready to watch? Enter your email to create or restart your membership.</OptForm.Text>
       </OptForm>
     </Accordion>
   )
