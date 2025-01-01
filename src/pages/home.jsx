fe0a3e06a1412a5e29f307beb4a4fb629cb22c36
Mouwafic Badarou <badaroumouwafic@gmail.com>
2025-01-01 18:33:10 +0100

    Integrate Feature component into Home: Embed Feature section with title and subtitle in the Home component, enhancing user engagement and visual appeal. This update includes the OptForm for email input within the Feature, improving the overall layout and user experience.

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,20 +2,24 @@ import { FaqsContainer } from "../containers/faqs"
 import { JumbotronContainer } from "../containers/jumbotron"
 import { FooterContainer } from "../containers/footer"
 import { HeaderContainer} from "../containers/header"
-import { OptForm } from "../components"
+import { Feature, OptForm } from "../components"
 
 export default function Home() {
   return (
     <>
       <HeaderContainer>
         <OptForm>
-          <OptForm.Input placeholder="Email Address" />
-          <OptForm.Button>Try it Now</OptForm.Button>
-          <OptForm.Break />
-          <OptForm.Text>
-            Ready to watch? Enter your email to create or restart your
-            membership.
-          </OptForm.Text>
+          <Feature>
+            <Feature.Title>Unlimited films, TV programmes and more.</Feature.Title>
+            <Feature.SubTitle>Watch anywhere. Cancel at any time.</Feature.SubTitle>
+            <OptForm.Input placeholder="Email Address" />
+            <OptForm.Button>Try it Now</OptForm.Button>
+            <OptForm.Break />
+            <OptForm.Text>
+              Ready to watch? Enter your email to create or restart your
+              membership.
+            </OptForm.Text>
+          </Feature>
         </OptForm>
       </HeaderContainer>
 
