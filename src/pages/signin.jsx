c33f947c7fbb736786cb1e4e9ced3db8748e5c93
Mouwafic Badarou <badaroumouwafic@gmail.com>
2025-01-02 13:27:04 +0100

    Implement sign-in form functionality: Enhance the Signin component in signin.jsx by adding state management for email and password inputs, and implement a handleSignin function to manage form submission. This update improves user interaction by allowing users to enter their credentials and submit the form, while maintaining error handling and layout consistency.

--- src/pages/signin.jsx
+++ src/pages/signin.jsx
@@ -4,12 +4,44 @@ import { Form } from "../components"
 
 export default function Signin() {
   const [error, setError] = useState('');
+  const [emailAddress, setEmailAddress] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleSignin = (event) => {
+    event.preventDefault();
+
+  }
   return (
     <HeaderContainer>
       <Form>
         <Form.Title>Sign In</Form.Title>
         {error && <Form.Error>{error}</Form.Error>}
+
+        <Form.Base onSubmit={handleSignin} method="POST">
+          <Form.Input
+            placeholder="Email Adress"
+            value={emailAddress}
+            onChange={({ target }) => setEmailAddress(target.value)}
+          />
+          <Form.Input
+            type="password"
+            placeholder="Password"
+            autoComplete="off"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+          <Form.Submit disabled={false} type="submit">
+            Sign In
+          </Form.Submit>
+
+          <Form.Text>
+            New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA.
+          </Form.TextSmall>
+        </Form.Base>
       </Form>
     </HeaderContainer>
-  )
+  );
 }
