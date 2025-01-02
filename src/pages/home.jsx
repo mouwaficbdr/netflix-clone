66ba46e6b2b1b26397f485441e050b7eb0dc7366
Mouwafic Badarou <badaroumouwafic@gmail.com>
2025-01-02 04:46:43 +0100

    Refactor OptForm placement in Home component: Move OptForm inside the Feature section for improved layout and user experience. This change enhances the visual structure of the Home page while maintaining functionality.

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -8,19 +8,19 @@ export default function Home() {
   return (
     <>
       <HeaderContainer>
-        <OptForm>
           <Feature>
             <Feature.Title>Unlimited films, TV programmes and more.</Feature.Title>
             <Feature.SubTitle>Watch anywhere. Cancel at any time.</Feature.SubTitle>
-            <OptForm.Input placeholder="Email Address" />
-            <OptForm.Button>Try it Now</OptForm.Button>
-            <OptForm.Break />
-            <OptForm.Text>
-              Ready to watch? Enter your email to create or restart your
-              membership.
-            </OptForm.Text>
+            <OptForm>
+                <OptForm.Input placeholder="Email Address" />
+                <OptForm.Button>Try it Now</OptForm.Button>
+                <OptForm.Break />
+                <OptForm.Text>
+                  Ready to watch? Enter your email to create or restart your
+                  membership.
+                </OptForm.Text>
+            </OptForm>
           </Feature>
-        </OptForm>
       </HeaderContainer>
 
       <JumbotronContainer />
