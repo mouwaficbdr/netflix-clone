633fc80b51b54fd2cfe2634cf4e525750eb7c6aa
Mouwafic Badarou <badaroumouwafic@gmail.com>
2025-01-02 12:11:27 +0100

    Integrate Firebase into application: Add Firebase initialization in main.jsx and create FirebaseContext for context management. This update enhances the app's functionality by enabling Firebase services.

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { GlobalStyles } from './global-styles.jsx'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { GlobalStyles } from './global-styles.jsx';
+import App from './App.jsx';
+import { initializeApp } from 'firebase/app';
+import { FirebaseContext } from './context/firebase.jsx';
+
+const config = {
+  apiKey: 'AIzaSyC_BC2ov54e0IjiUwIjZiEY4MUyZEEyeao',
+  authDomain: 'netflix-clone-1adf4.firebaseapp.com',
+  projectId: 'netflix-clone-1adf4',
+  storageBucket: 'netflix-clone-1adf4.firebasestorage.app',
+  messagingSenderId: '739993842657',
+  appId: '1:739993842657:web:60f540c741f3be777bd8a0',
+};
+
+const firebase = initializeApp(config)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GlobalStyles/>
-    <App />
-  </StrictMode>,
-)
+    <FirebaseContext.Provider value={{ firebase: window.firebase }}>
+      <GlobalStyles />
+      <App />
+    </FirebaseContext.Provider>
+  </StrictMode>
+);
