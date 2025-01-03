7572d426e7b856c3eed5e3aab579a7c59d5009e5
Mouwafic Badarou <badaroumouwafic@gmail.com>
2025-01-03 17:12:33 +0100

    Enhance Header component with search functionality and new styled elements: Introduced a search feature with state management for search term input, allowing users to search for films and series. Added new styled components Feature, FeatureCallOut, and PlayButton to improve modularity and layout flexibility in the Header. This update enhances user experience and overall structure of the Header component.

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,6 +1,21 @@
 /* eslint-disable react/prop-types */
+import { useState } from 'react';
 import { Link as ReachRouterLink } from 'react-router-dom'
-import { Background, Container, Logo, ButtonLink, Group, Link, Text } from './styles/header'
+import {
+  Background,
+  Container,
+  Logo,
+  ButtonLink,
+  Group,
+  Link,
+  Text,
+  Feature,
+  FeatureCallOut,
+  PlayButton,
+  SearchIcon,
+  SearchInput,
+  Search
+} from './styles/header'
 
 export default function Header({ bg = true, children, ...restProps }) {
   return bg ? <Background {...restProps}>{children}</Background> : children;
@@ -12,6 +27,23 @@ Header.Frame = function HeaderFrame({ children, ...restProps }) {
 
 Header.Group = function HeaderGroup({ children, ...restProps }) {
   return <Group {...restProps}>{children}</Group>
+}
+
+Header.Search = function HeaderSearch({ searchTerm, setSearchTerm, ...restProps }) {
+  const [searchActive, setSearchActive] = useState(false)
+  return (
+    <Search {...restProps} >
+      <SearchIcon onClick={() => setSearchActive(!searchActive)}>
+        <img src='/images/icons/search.png' alt='Search' />
+      </SearchIcon>
+      <SearchInput
+        value={searchTerm}
+        onChange={({ target }) => setSearchTerm(target.value)}
+        placeholder="Search films and series"
+        active={searchActive}
+      />
+    </Search >
+  )
 }
 
 Header.Logo = function HeaderLogo({ to, ...restProps }) {
@@ -32,4 +64,16 @@ Header.Link = function HeaderLink({ children, ...restProps }) {
 
 Header.Text = function HeaderText({ children, ...restProps }) {
   return <Text {...restProps}>{children}</Text>
+}
+
+Header.Feature = function HeaderFeature({ children, ...restProps }) {
+  return <Feature {...restProps}>{children}</Feature>
+}
+
+Header.FeatureCallOut = function HeaderFeatureCallOut({ children, ...restProps }) {
+  return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>
+}
+
+Header.PlayButton = function HeaderPlayButton({ children, ...restProps }) {
+  return <PlayButton {...restProps}>{children}</PlayButton>
 }
