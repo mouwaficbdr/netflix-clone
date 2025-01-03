5ea2e0d9370b558b2801a7287f7dbdfb2d363f8d
Mouwafic Badarou <badaroumouwafic@gmail.com>
2025-01-03 17:13:09 +0100

    Enhance BrowseContainer with search functionality and feature display: Added state management for search term input and integrated a new Feature section showcasing the "Joker" film. Updated the Header to include a search component, improving user interaction and content discovery. This update enhances the overall user experience while browsing films and series.

--- src/containers/browse.jsx
+++ src/containers/browse.jsx
@@ -1,15 +1,16 @@
 import { useContext, useState } from "react"
 import { Header } from "../components"
 import * as ROUTES from "../constants/routes"
-// import { FirebaseContext } from "../context/firebase"
+import { FirebaseContext } from "../context/firebase"
 import { SelectProfileContainer } from "./profiles"
 import { FooterContainer } from "./footer"
+
 
 export function BrowseContainer() {
   const [profile, setProfile] = useState({})
   const [category, setCategory] = useState("series")
   const [loading, setLoading] = useState(true)
-
+  const [searchTerm, setSearchTerm] = useState("")
   // const { firebase } = useContext(FirebaseContext)
 
   const user = {
@@ -22,18 +23,44 @@ export function BrowseContainer() {
       <Header src="joker1" dontShowOnSmallViewPort>
         <Header.Frame>
           <Header.Group>
-            <Header.Logo to={ROUTES.HOME} src="/images/misc/logo.svg" alt="Netflix" />
-            <Header.Link active={category === "series" ? "true" : "false"} onClick={() => setCategory("series")}>
+            <Header.Logo
+              to={ROUTES.HOME}
+              src="/images/misc/logo.svg"
+              alt="Netflix"
+            />
+            <Header.Link
+              active={category === 'series' ? 'true' : 'false'}
+              onClick={() => setCategory('series')}
+            >
               Series
             </Header.Link>
-            <Header.Link active={category === "films" ? "true" : "false"} onClick={() => setCategory("films")}>
+            <Header.Link
+              active={category === 'films' ? 'true' : 'false'}
+              onClick={() => setCategory('films')}
+            >
               Films
             </Header.Link>
           </Header.Group>
+          <Header.Group>
+            <Header.Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+          </Header.Group>
         </Header.Frame>
+  
+        <Header.Feature>
+          <Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
+          <Header.Text>
+            Forever alone in a crowd, failed comedian Arthur Fleck seeks
+            connection as he walks the streets of Gotham City. Arthur wears two
+            masks -- the one he paints for his day job as a clown, and the guise
+            he projects in a futile attempt to feel like he is part of the world
+            around him.
+          </Header.Text>
+          <Header.PlayButton>Play</Header.PlayButton>
+        </Header.Feature>
       </Header>
       <FooterContainer />
-    </>)
-    : 
-    (<SelectProfileContainer user={user} setProfile={setProfile} />)
+    </>
+  ) : (
+    <SelectProfileContainer user={user} setProfile={setProfile} />
+  );
 }
