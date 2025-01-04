55c4e2eeea621a66135d9df02d7f021de6cfb391
Mouwafic Badarou <badaroumouwafic@gmail.com>
2025-01-04 17:29:00 +0100

    Integrate useContent hook in Browse component: Added useContent hook to fetch series and films data, enhancing content retrieval. Included console logs for debugging purposes to verify data fetching.

--- src/pages/browse.jsx
+++ src/pages/browse.jsx
@@ -1,6 +1,11 @@
 import { BrowseContainer } from "../containers/browse";
-
+import { useContent } from "../hooks";
 
 export default function Browse() {
+  const { series } = useContent("series")
+  const { films } = useContent("films")
+
+  console.log(series)
+  console.log(films)
   return <BrowseContainer />
 }
