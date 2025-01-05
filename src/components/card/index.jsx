f1d891915f2a507f94385cb7dbe52a7b44702228
Mouwafic Badarou <badaroumouwafic@gmail.com>
2025-01-05 08:17:11 +0100

    Refactor Card component for improved readability and functionality: Standardized import statements, enhanced context provider structure, and improved formatting across subcomponents. Fixed a typo in the feature description and adjusted layout properties for better visual consistency. This update enhances code clarity and maintains a consistent coding style.

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,102 +1,110 @@
 /* eslint-disable react/prop-types */
-import { useState, useContext, createContext } from 'react'
+import{ useState, useContext, createContext } from 'react';
 
 import {
-  Container, 
+  Container,
   Group,
-  Title, 
+  Title,
   SubTitle,
   Text,
   Feature,
   FeatureTitle,
-  FeatureClose,
   FeatureText,
+  FeatureClose,
   Maturity,
   Content,
   Meta,
   Entities,
   Item,
-  Image
-} from "./styles/card"
+  Image,
+} from './styles/card';
 
 const FeatureContext = createContext();
 
 export default function Card({ children, ...restProps }) {
-
-  const [showFeature, setShowFeature] = useState(false)
-  const [itemFeature ,setItemFeature] = useState(false)
+  const [showFeature, setShowFeature] = useState(false);
+  const [itemFeature, setItemFeature] = useState(false);
 
   return (
-    <FeatureContext.Provider value={showFeature, setShowFeature, itemFeature, setItemFeature}>
-      <Container {...restProps} >{children}</Container>
+    <FeatureContext.Provider
+      value={{ showFeature, setShowFeature, itemFeature, setItemFeature }}
+    >
+      <Container {...restProps}>{children}</Container>
     </FeatureContext.Provider>
-  )
+  );
 }
 
 Card.Group = function CardGroup({ children, ...restProps }) {
-  return <Group {...restProps} >{ children }</Group>
-}
+  return <Group {...restProps}>{children}</Group>;
+};
 
 Card.Title = function CardTitle({ children, ...restProps }) {
-  return <Title {...restProps} >{ children }</Title>
-}
+  return <Title {...restProps}>{children}</Title>;
+};
 
 Card.SubTitle = function CardSubTitle({ children, ...restProps }) {
-  return <SubTitle {...restProps} >{ children }</SubTitle>
-}
+  return <SubTitle {...restProps}>{children}</SubTitle>;
+};
 
 Card.Text = function CardText({ children, ...restProps }) {
-  return <Text {...restProps} >{ children }</Text>
-}
+  return <Text {...restProps}>{children}</Text>;
+};
 
 Card.Entities = function CardEntities({ children, ...restProps }) {
-  return <Entities {...restProps} >{ children }</Entities>
-}
+  return <Entities {...restProps}>{children}</Entities>;
+};
 
 Card.Meta = function CardMeta({ children, ...restProps }) {
-  return <Meta {...restProps} >{ children }</Meta>
-}
+  return <Meta {...restProps}>{children}</Meta>;
+};
 
 Card.Item = function CardItem({ item, children, ...restProps }) {
-  const {setShowFeature, setItemFeature} = useContext(FeatureContext)
+  const { setShowFeature, setItemFeature } = useContext(FeatureContext);
+
   return (
-    <Item {...restProps}
+    <Item
       onClick={() => {
-        setItemFeature(item)
+        setItemFeature(item);
         setShowFeature(true);
       }}
+      {...restProps}
     >
       {children}
     </Item>
-  ) 
-}
+  );
+};
 
 Card.Image = function CardImage({ ...restProps }) {
-  return <Image {...restProps} />
-}
+  return <Image {...restProps} />;
+};
 
 Card.Feature = function CardFeature({ children, category, ...restProps }) {
-  const { showFeature, itemFeature, setShowFeature } = useContext(FeatureContext)
-  
+  const { showFeature, itemFeature, setShowFeature } =
+    useContext(FeatureContext);
+
   return showFeature ? (
-    <Feature src={`/images/${category}/${itemFeature.genre}/${itemFeature.slug}/large.jpg`}>
+    <Feature
+      src={`/images/${category}/${itemFeature.genre}/${itemFeature.slug}/large.jpg`}
+    >
       <Content>
         <FeatureTitle>{itemFeature.title}</FeatureTitle>
-        <FeatureText>{itemFeature.descripition}</FeatureText>
+        <FeatureText>{itemFeature.description}</FeatureText>
         <FeatureClose onClick={() => setShowFeature(false)}>
-          <img src='/images/icons/close.png' alt='close' />
+          <img src="/images/icons/close.png" alt="Close" />
         </FeatureClose>
-        <Group margin="20px 0" flexDirection="row" alignItems="center">
-          <Maturity rating={itemFeature.maturity} >
-            {itemFeature.maturity < 12 ? "PG" : itemFeature.maturity}
+
+        <Group margin="30px 0" flexDirection="row" alignItems="center">
+          <Maturity rating={itemFeature.maturity}>
+            {itemFeature.maturity < 12 ? 'PG' : itemFeature.maturity}
           </Maturity>
           <FeatureText fontWeight="bold">
-            {itemFeature.genre.charAt(0).toUpperCase() + itemFeature.genre.slice(1)}
+            {itemFeature.genre.charAt(0).toUpperCase() +
+              itemFeature.genre.slice(1)}
           </FeatureText>
         </Group>
 
         {children}
       </Content>
     </Feature>
-  ) : null
-}
+  ) : null;
+};
