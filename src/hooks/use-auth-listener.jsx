a306ca84c082ed8cce7d0a5b5c82136e07011aff
Mouwafic Badarou <badaroumouwafic@gmail.com>
2025-01-07 08:24:53 +0100

    Refactor useAuthListener hook to optimize user state initialization and enhance authentication listener
    This commit improves the useAuthListener hook by initializing the user state from localStorage using a callback for better performance. It also adds a check to ensure the Firebase auth instance is defined before setting up the authentication listener. The cleanup function for the listener is updated for clarity, ensuring proper resource management when the component unmounts. These changes enhance the efficiency and reliability of user authentication handling.

--- src/hooks/use-auth-listener.jsx
+++ src/hooks/use-auth-listener.jsx
@@ -3,28 +3,33 @@ import { FirebaseContext } from '../context/firebase';
 import { getAuth, onAuthStateChanged } from 'firebase/auth'; // Nouvelle API modulaire
 
 export default function useAuthListener() {
+  // Initialiser l'utilisateur à partir de localStorage (optimisation avec callback)
   const [user, setUser] = useState(() => {
-    // Initialisation du state depuis localStorage avec un callback pour plus de performance
     return JSON.parse(localStorage.getItem('authUser'));
   });
 
   const { firebase } = useContext(FirebaseContext);
-  const auth = getAuth(firebase); // Obtenir l'instance d'authentification de Firebase
+  const auth = getAuth(firebase); // Obtenir l'instance d'authentification Firebase
 
   useEffect(() => {
-    const listener = onAuthStateChanged(auth, (authUser) => {
+    if (!auth) return; // Si auth n'est pas encore défini, ne pas exécuter l'effet
+
+    // Écouter les changements d'état d'authentification
+    const unsubscribe = onAuthStateChanged(auth, (authUser) => {
       if (authUser) {
+        // L'utilisateur est connecté
         localStorage.setItem('authUser', JSON.stringify(authUser));
         setUser(authUser);
       } else {
+        // L'utilisateur est déconnecté
         localStorage.removeItem('authUser');
         setUser(null);
       }
     });
 
-    // Cleanup de l'écouteur lorsque le composant est démonté
-    return () => listener();
-  }, [auth]); // Auth est une dépendance, il peut changer (utile si Firebase est reconfiguré)
+    // Nettoyer l'écouteur lorsque le composant est démonté
+    return unsubscribe;
+  }, [auth]); // Exécuter cet effet uniquement lorsque "auth" change
 
   return { user };
 }
