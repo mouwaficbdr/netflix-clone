e655c52fe49baa37683903a710767df56cf80824
Mouwafic Badarou <badaroumouwafic@gmail.com>
2025-01-07 08:24:12 +0100

    Refactor App component to integrate authentication and route protection
    This commit enhances the App component by incorporating the useAuthListener hook for managing user authentication state. It implements IsUserRedirect for sign-in, sign-up, and home routes to redirect authenticated users to the browse page. Additionally, it introduces a ProtectedRoute for the browse route, ensuring that only authenticated users can access it. These changes improve user experience by providing appropriate access control based on authentication status.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,50 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import * as ROUTES from './constants/routes'
-import { Home, Signin, Signup, Browse } from './pages'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import * as ROUTES from './constants/routes';
+import { Home, Signin, Signup, Browse } from './pages';
+import { useAuthListener } from './hooks';
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
 
 export default function App() {
+  const { user } = useAuthListener();
+
   return (
     <Router>
       <Routes>
+        {/* Redirection si l'utilisateur est déjà connecté */}
         <Route
           path={ROUTES.SIGN_IN}
-          element={<Signin />}
+          element={
+            <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE}>
+              <Signin />
+            </IsUserRedirect>
+          }
         />
         <Route
           path={ROUTES.SIGN_UP}
-          element={<Signup />}
+          element={
+            <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE}>
+              <Signup />
+            </IsUserRedirect>
+          }
         />
+        {/* Route protégée : nécessite une connexion */}
         <Route
           path={ROUTES.BROWSE}
-          element={<Browse />}
+          element={
+            <ProtectedRoute user={user}>
+              <Browse />
+            </ProtectedRoute>
+          }
         />
-        <Route path={ROUTES.HOME} element={<Home />} />
+        {/* Redirection si l'utilisateur est déjà connecté */}
+        <Route
+          path={ROUTES.HOME}
+          element={
+            <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE}>
+              <Home />
+            </IsUserRedirect>
+          }
+        />
       </Routes>
     </Router>
   );
