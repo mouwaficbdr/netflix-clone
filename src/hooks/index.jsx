05076e2891b5dd1a1f217e2c3c1cda2d26a03b11
Mouwafic Badarou <badaroumouwafic@gmail.com>
2025-01-08 21:40:54 +0100

    Added the new exports for new hooks

--- src/hooks/index.jsx
+++ src/hooks/index.jsx
@@ -1,2 +1,4 @@
-export { default as useContent } from "./use-content"
+export { default as useGenres } from "./use-genres"
+export { default as useMovies } from "./use-movies"
+export { default as useSeries } from "./use-series"
 export { default as useAuthListener} from "./use-auth-listener"
