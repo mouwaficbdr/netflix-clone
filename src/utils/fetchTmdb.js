b47cb8b43b04ab904b3bd5cf8b6cd533c1c1b7cc
Mouwafic Badarou <badaroumouwafic@gmail.com>
2025-01-09 16:49:26 +0100

    Refactor fetchSeries and fetchMovies functions to support pagination and return all results. Enhanced error handling with detailed status messages.

--- src/utils/fetchTmdb.js
+++ src/utils/fetchTmdb.js
@@ -4,39 +4,59 @@ const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 // Base URL de TMDB
 const BASE_URL = 'https://api.themoviedb.org/3';
 
-export async function fetchSeries() {
-  try {
-    const response = await fetch(
-      `${BASE_URL}/discover/tv?api_key=${API_KEY}&include_adult=false&include_null_first_air_dates=false&language=fr-FR&page=1&sort_by=popularity.desc`
-    );
+export async function fetchSeries(totalPages = 10) {
 
-    if (!response.ok) {
-      throw new Error("Erreur lors de la récupération des séries")
+  const allResults = [];
+  
+  try {
+
+    for (let page = 1; page <= totalPages; page++) {
+      
+      const response = await fetch(
+        `${BASE_URL}/discover/tv?api_key=${API_KEY}&include_adult=false&include_null_first_air_dates=false&language=fr-FR&page=${page}&sort_by=popularity.desc`
+      );
+  
+      if (!response.ok) {
+        throw new Error(`Erreur: ${response.status} - ${response.statusText}`);
+      }
+
+      const data = await response.json()
+      allResults.push(...data.results)
     }
 
-    const data = await response.json()
-    return data
+    console.log("Toutes les séries: ", allResults)
+    return allResults;
 
   } catch (error) {
     console.error("Erreur avec fetchSeries: ", error)
   }
 }
 
-export async function fetchMovies() {
+export async function fetchMovies(totalPages = 10) {
+
+  const allResults = [];
+  
   try {
-    const response = await fetch(
-      `${BASE_URL}/discover/movie?api_key=${API_KEY}&include_adult=false&include_null_first_air_dates=false&language=fr-FR&page=1&sort_by=popularity.desc`
-    );
 
-    if (!response.ok) {
-      throw new Error('Erreur lors de la récupération des films');
-    }  
+    for (let page = 1; page <= totalPages; page++) {
+      
+      const response = await fetch(
+        `${BASE_URL}/discover/movie?api_key=${API_KEY}&include_adult=false&include_null_first_air_dates=false&language=fr-FR&page=${page}&sort_by=popularity.desc`
+      );
+  
+      if (!response.ok) {
+        throw new Error(`Erreur: ${response.status} - ${response.statusText}`);
+      }
 
-    const data = await response.json()
-    return data
+      const data = await response.json()
+      allResults.push(...data.results)
+    }
+
+    console.log("Tous les films: ", allResults)
+    return allResults;
 
   } catch (error) {
-    console.log("Erreur avec fetch movies: ", error)
+    console.error("Erreur avec fetchMovies: ", error)
   }
 }
 
