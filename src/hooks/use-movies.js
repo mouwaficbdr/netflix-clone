a4a093ff1cce02f0f9fa14b11f10440ab5eb2b34
Mouwafic Badarou <badaroumouwafic@gmail.com>
2025-01-09 16:37:18 +0100

    Update useMovies hook to set movies data directly from API response

--- src/hooks/use-movies.js
+++ src/hooks/use-movies.js
@@ -10,7 +10,7 @@ export default function useMovies() {
       try {
         setLoading(true);
         const data = await fetchMovies();
-        setMovies(data.results);
+        setMovies(data);
         console.log("Les films récupérés: ", data)
       } catch (error) {
         console.error('Erreur lors de la récupération des séries :', error);
