799df4297c334114a9bb2b9f8724b41129a81704
Mouwafic Badarou <badaroumouwafic@gmail.com>
2025-01-13 16:30:44 +0100

    Enhance HeaderContainer to accept additional props for ButtonLink component
    - Updated HeaderContainer to spread additional props onto the Sign In ButtonLink for improved flexibility.
    - Cleaned up the code for better readability and maintainability.

--- src/containers/header.jsx
+++ src/containers/header.jsx
@@ -2,12 +2,12 @@
 import { Header } from "../components"
 import * as ROUTES from "../constants/routes"
 
-export function HeaderContainer({ children }) {
+export function HeaderContainer({ children, ...restProps }) {
   return (
-    <Header>
+    <Header >
       <Header.Frame>
         <Header.Logo to={ROUTES.HOME} src="/images/misc/logo.svg" alt="Netflix"/>
-        <Header.ButtonLink to={ROUTES.SIGN_IN}>Sign  In</Header.ButtonLink>
+        <Header.ButtonLink to={ROUTES.SIGN_IN} {...restProps}>Sign  In</Header.ButtonLink>
       </Header.Frame>
       {children}
     </Header>
