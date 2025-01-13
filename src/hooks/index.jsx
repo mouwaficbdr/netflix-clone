01f2f66351194c363d4e40f96f62a820438c85c1
Mouwafic Badarou <badaroumouwafic@gmail.com>
2025-01-13 13:35:04 +0100

    Enhance hooks export in index.jsx
    - Added export for truncateText hook to improve text handling capabilities.
    - Cleaned up formatting for useAuthListener export for consistency.

--- src/hooks/index.jsx
+++ src/hooks/index.jsx
@@ -1,4 +1,5 @@
 export { default as useGenres } from "./use-genres"
 export { default as useMovies } from "./use-movies"
 export { default as useSeries } from "./use-series"
-export { default as useAuthListener} from "./use-auth-listener"
+export { default as useAuthListener } from "./use-auth-listener"
+export { default as truncateText } from "./truncate-text"
